ac668b7fb3d5370221f3d0fcceccc897e9963b68
Kibiwott <bk.kibiwott@gmail.com>
2025-05-13 18:53:37 +0300

    Made the bot respond to firstAid questions only

--- controllers/ChatBot.Cotroller.js
+++ controllers/ChatBot.Cotroller.js
@@ -6,10 +6,16 @@ const ai = new GoogleGenAI({
 
 export const chatBot = async (req, res) => {
   const { prompt } = req.body;
+    const firstAidInstruction = `
+        You are a helpful assistant who ONLY answers questions related to FIRST AID.
+        If the question is not about first aid, kindly respond:
+        "I'm here to assist with first aid questions only. Please ask me something related to emergency medical care or basic first aid treatment."
+
+        Here is the user's question:`;
   try {
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
-      contents: [{ role: "user", parts: [{ text: prompt }] }], 
+      contents: [{ role: "user", parts: [{ text: firstAidInstruction + prompt }] }], 
     });
    
     const responseText = response.candidates[0]?.content?.parts[0]?.text;
